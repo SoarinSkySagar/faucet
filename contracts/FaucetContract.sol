da5a501e0f3b82c7db64d82a34852b88d3530c42
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-08-17 20:08:43 +0530

    initial commit

---
+++ contracts/FaucetContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Faucet {
+    uint public funds = 1000;
+    int public counter = -10;
+    uint32 public test = 4294967295;
+}
---
+++ migrations/1_deploy_contracts.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+const ConvertLib = artifacts.require("ConvertLib");
+const MetaCoin = artifacts.require("MetaCoin");
+
+module.exports = function(deployer) {
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
+};

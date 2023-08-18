bb8c3ccabbb478ca2f40eec8668583d13f9a4007
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-08-18 21:01:23 +0530

    added contract functions

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,11 +2,31 @@
 pragma solidity ^0.8.13;
 
 contract Faucet {
+    address[] public funders;
+
     receive() external payable {}
 
-    function addFunds() external payable {}
+    function addFunds() external payable {
+        bool exists = false;
+        for (uint256 i = 0; i < funders.length; i++) {
+            if (funders[i] == msg.sender) {
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            funders.push(msg.sender);
+        }
+    }
 
-    function test() external pure returns(uint) {
-        return 2 + 2;
+    function getAllFunders() public view returns (address[] memory) {
+        return funders;
+    }
+
+    function getFunderIndex(uint index) external view returns (address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
     }
 }
+
+// const instance = await Faucet.deployed()

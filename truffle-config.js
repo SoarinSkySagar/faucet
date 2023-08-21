49ac540dc4c62eac16b23720544dfcbb9ecea929
SoarinSkySagar <sagarrana.pvt@gmail.com>
2023-08-22 01:45:33 +0530

    deployed to sepolia testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "custom update chuckle find ivory sense drink arrest burden grid saddle bread";
+const infuraID = "f84a400bb4bc4157bce382f7ed45e199";
+
 module.exports = {
   contracts_build_directory: "./public/contracts",
   networks: {
@@ -7,9 +11,9 @@ module.exports = {
      network_id: "*",
     },
     sepolia: {
-      host: "https://sepolia.infura.io/v3/",
-      network_id: "11155111",
-    },
+      provider: () => new HDWalletProvider(mnemonic, `https://sepolia.infura.io/v3/${infuraID}`),
+      network_id: 11155111,
+    }
   },
   compilers: {
     solc: {
